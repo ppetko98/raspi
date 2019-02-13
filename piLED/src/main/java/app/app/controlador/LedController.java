0f58c93c39f64844c98abdc6fb64b711e1ee2c5f
ppetko98 <Ppetko98@gmail.com>
2019-02-13 12:24:32 +0100

    start

---
+++ piLED/src/main/java/app/app/controlador/LedController.java
@@ -0,0 +1,26 @@
+package app.app.controlador;
+
+import com.pi4j.io.gpio.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class LedController {
+
+    private static GpioPinDigitalOutput pin;
+
+    @RequestMapping("/")
+    public String hola(){
+        return "Hello world";
+    }
+
+    @RequestMapping("/toggle")
+    public String light(){
+        if (pin==null) {
+            GpioController gpioController = GpioFactory.getInstance();
+            pin = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01, "LED1", PinState.LOW);
+        }
+
+        pin.toggle();
+        return "OK";
+    }
+
+}
